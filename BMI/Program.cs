60dcf7f13225a6ac94ea8d180850692385dbc963
Čejka David <Cejka.David@stud.vassboskovice.cz>
2021-10-19 09:03:52 +0200

    Přidejte soubory projektu.

---
+++ BMI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Výpočet BMI");
+            
+            Console.Read();
+        }
+    }
+}
